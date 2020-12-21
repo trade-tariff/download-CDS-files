e91a48503fa3ab367ecbe0ef1bd7c9ec71c0dad6
Matt Lavis <matthew.lavis@gmail.com>
2020-12-21 11:27:24 +0000

    First commit

---
+++ get_token.py
@@ -0,0 +1,34 @@
+import requests
+import sys
+import os
+from dotenv import load_dotenv
+
+# Get credentials
+load_dotenv('.env')
+
+domain = os.getenv('domain')
+client_secret = os.getenv('client_secret')
+client_id = os.getenv('client_id')
+
+# Request auth token
+url = domain + "oauth/token"
+payload = "client_secret={}\&client_id={}&grant_type=client_credentials".format(client_secret, client_id)
+headers = {"content-type": "application/x-www-form-urlencoded"}
+response = requests.request("POST", url, data = payload, headers = headers)
+if response.status_code != 200:
+    print(response.text)
+    sys.exit()
+
+my_array = response.json()
+access_token = my_array["access_token"]
+
+# Access data
+url = domain + "bulk-data-download/list/TARIFF-MONTHLY"
+headers = {
+    "User-Agent": "Trade Tariff Backend",
+    "Accept": "application/vnd.hmrc.1.0+json",
+    "Authorization": "Bearer " + access_token
+}
+response = requests.request("GET", url, headers=headers)
+print(response.text)
+print(response.status_code)
