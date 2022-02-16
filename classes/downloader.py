c395b12566801b4a673b4e40847c8ef5df95f1e8
mattlavis-transform <matthew.lavis@gmail.com>
2022-02-16 19:57:52 +0000

    Additional periods

--- classes/downloader.py
+++ classes/downloader.py
@@ -85,3 +85,109 @@ class Downloader(object):
                             print("There was a problem in unzipping that archive.")
                     except:
                         print("Failed attempt to download file from", download_url, file_entry.filename)
+
+    def download_files_monthly(self):
+        # Access data
+        url = self.domain + "bulk-data-download/list/TARIFF-MONTHLY"
+        headers = {
+            "User-Agent": "Trade Tariff Backend",
+            "Accept": "application/vnd.hmrc.1.0+json",
+            "Authorization": "Bearer " + self.access_token
+        }
+        response = requests.request("GET", url, headers=headers)
+
+        files = response.json()
+        for file_entry in files:
+            cds_file = CdsFile()
+            cds_file.filename = file_entry['filename']
+            cds_file.download_url = file_entry['downloadURL']
+            self.cds_files.append(cds_file)
+            print(file_entry['filename'])
+
+        # sys.exit()
+        self.cds_files = sorted(self.cds_files, key=lambda x: x.filename, reverse=True)
+
+        for file_entry in self.cds_files:
+            resource_path = os.path.join(os.getcwd(), "resources")
+            zip_path = os.path.join(resource_path, "zip")
+            xml_path = os.path.join(resource_path, "xml_monthly")
+            filename = file_entry.filename
+            if "gzip" in filename:
+                download_url = file_entry.download_url
+                zip_filename = os.path.join(zip_path, filename)
+                
+                if os.path.isfile(zip_filename):
+                    print(f'{filename} already exists, skipping...')
+                else:
+                    print(f'Downloading {filename}...')
+                    try:
+                        wget.download(download_url, out=zip_path, bar=None)
+                        zfile = zipfile.ZipFile(zip_filename)
+                        zfile.extractall(xml_path)
+                        unzipped_files = zfile.filelist
+                        if unzipped_files:
+                            xml_filename = unzipped_files[0].filename
+
+                            # Copy to the import folder for running the import
+                            src = os.path.join(xml_path, xml_filename)
+                            dest = os.path.join(self.IMPORT_FOLDER, "CDS")
+                            dest = os.path.join(dest, xml_filename)
+                            copyfile(src, dest)
+                        else:
+                            print("There was a problem in unzipping that archive.")
+                    except:
+                        print("Failed attempt to download file from", download_url, file_entry.filename)
+
+    def download_files_annual(self):
+        # Access data
+        url = self.domain + "bulk-data-download/list/TARIFF-ANNUAL"
+        headers = {
+            "User-Agent": "Trade Tariff Backend",
+            "Accept": "application/vnd.hmrc.1.0+json",
+            "Authorization": "Bearer " + self.access_token
+        }
+        response = requests.request("GET", url, headers=headers)
+
+        files = response.json()
+        for file_entry in files:
+            cds_file = CdsFile()
+            cds_file.filename = file_entry['filename']
+            cds_file.download_url = file_entry['downloadURL']
+            self.cds_files.append(cds_file)
+            print(file_entry['filename'])
+
+        # sys.exit()
+        self.cds_files = sorted(self.cds_files, key=lambda x: x.filename, reverse=True)
+
+        for file_entry in self.cds_files:
+            resource_path = os.path.join(os.getcwd(), "resources")
+            zip_path = os.path.join(resource_path, "zip")
+            xml_path = os.path.join(resource_path, "xml_annual")
+            filename = file_entry.filename
+            if "gzip" in filename:
+                download_url = file_entry.download_url
+                zip_filename = os.path.join(zip_path, filename)
+                
+                if os.path.isfile(zip_filename):
+                    print(f'{filename} already exists, skipping...')
+                else:
+                    print(f'Downloading {filename}...')
+                    try:
+                        wget.download(download_url, out=zip_path, bar=None)
+                        zfile = zipfile.ZipFile(zip_filename)
+                        zfile.extractall(xml_path)
+                        unzipped_files = zfile.filelist
+                        if unzipped_files:
+                            xml_filename = unzipped_files[0].filename
+
+                            # Copy to the import folder for running the import
+                            src = os.path.join(xml_path, xml_filename)
+                            dest = os.path.join(self.IMPORT_FOLDER, "CDS")
+                            dest = os.path.join(dest, xml_filename)
+                            copyfile(src, dest)
+                        else:
+                            print("There was a problem in unzipping that archive.")
+                    except:
+                        print("Failed attempt to download file from", download_url, file_entry.filename)
+                        
+                        
---
+++ download_annual.py
@@ -0,0 +1,8 @@
+import sys
+from classes.downloader import Downloader
+from classes.parser import Parser
+import classes.globals as g
+
+
+d = Downloader()
+d.download_files_annual()
