c6c5ced613b7869d0ccb51f7dba1c6e6f4f43bf2
Matt Lavis <matthew.lavis@gmail.com>
2022-09-02 19:09:11 +0100

    Parameterise Xpath

--- xpath.py
+++ xpath.py
@@ -1,14 +1,26 @@
 import glob
+import os
+import sys
+from dotenv import load_dotenv
 from classes.xpath_query import XpathQuery
 from classes.xpath_markdown import XpathMarkdown
 
-query_class = "commodity"
-query_id = "2933199070"
+if len(sys.argv) < 3:
+    print("Provide class and instance")
+    sys.exit()
+else:
+    query_class = sys.argv[1]
+    query_id = sys.argv[2]
 
-query_class = "measure"
-query_id = "20138292"
+# query_class = "commodity"
+# query_id = "2933199070"
 
-folder = "/Users/mattlavis/sites and projects/1. Online Tariff/tariff data/DIT/"
+# query_class = "measure"
+# query_id = "20138292"
+
+load_dotenv('.env')
+folder = os.getenv('DIT_DATA_FOLDER')
+# folder = "/Users/mattlavis/sites and projects/1. Online Tariff/tariff data/DIT/"
 files = glob.glob(folder + '/*.xml')
 files = sorted(files)
 records = []
