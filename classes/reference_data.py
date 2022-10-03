0d494398f9984afa6e8573bb02936bf4d02f61f1
William Fish <william.michael.fish@gmail.com>
2022-10-03 17:15:11 +0100

    HOTT-2087: Migrate to api requests for reference data

--- classes/reference_data.py
+++ classes/reference_data.py
@@ -1,35 +1,83 @@
-from classes.database import Database
+import os
+import sys
+import requests
 
+from dotenv import load_dotenv
+
+load_dotenv(".env")
+
+
+class ReferenceDataHandler(object):
+    def __init__(self, path):
+        self._url = os.getenv("OTT_HOST") + path
+        self._headers = {"content-type": "application/json"}
+
+    def __enter__(self):
+        try:
+            response_json = requests.request(
+                "GET", self._url, headers=self._headers
+            ).json()
+
+            return response_json["data"]
+        except Exception:
+            print("Failed to download reference data", self._url)
+            sys.exit()
+
+    def __exit__(self, _a, _b, _c):
+        pass
+
+
+class GeographyList(object):
+    def __init__(self):
+        self.geography_dict = {}
+        self.geography_hjid_dict = {}
+
+    def load(self):
+        with ReferenceDataHandler(
+            "/api/v2/geographical_areas?filter['exclude_none']=true"
+        ) as geographical_areas:
+            for geographical_area in geographical_areas:
+                self.geography_dict[geographical_area["id"]] = geographical_area[
+                    "attributes"
+                ]["description"]
+
+                self.geography_hjid_dict[
+                    geographical_area["attributes"]["hjid"]
+                ] = geographical_area["attributes"]["description"]
 
 class MeasureTypeList(object):
     def __init__(self):
         self.measure_type_dict = {}
-        sql = """select mt.measure_type_id, mtd.description
-        from measure_types mt, measure_type_descriptions mtd 
-        where mt.measure_type_id = mtd.measure_type_id 
-        and validity_end_date is null
-        order by 1"""
-        d = Database()
-        rows = d.run_query(sql)
-        for row in rows:
-            self.measure_type_dict[row[0]] = row[1]
+
+    def load(self):
+        with ReferenceDataHandler("/api/v2/measure_types") as measure_types:
+            for measure_type in measure_types:
+                self.measure_type_dict[measure_type["id"]] = measure_type["attributes"][
+                    "description"
+                ]
 
 
 class ActionCodeList(object):
     def __init__(self):
         self.action_code_dict = {}
-        sql = """select action_code, description from measure_action_descriptions mad order by 1"""
-        d = Database()
-        rows = d.run_query(sql)
-        for row in rows:
-            self.action_code_dict[row[0]] = row[1]
+
+    def load(self):
+        with ReferenceDataHandler("/api/v2/measure_actions") as measure_actions:
+            for measure_action in measure_actions:
+                self.action_code_dict[measure_action["id"]] = measure_action[
+                    "attributes"
+                ]["description"]
 
 
 class ConditionCodeList(object):
     def __init__(self):
         self.condition_code_dict = {}
-        sql = """select condition_code, description from measure_condition_code_descriptions mccd order by 1"""
-        d = Database()
-        rows = d.run_query(sql)
-        for row in rows:
-            self.condition_code_dict[row[0]] = row[1]
+
+    def load(self):
+        with ReferenceDataHandler(
+            "/api/v2/measure_condition_codes"
+        ) as measure_condition_codes:
+            for measure_condition_code in measure_condition_codes:
+                self.condition_code_dict[
+                    measure_condition_code["id"]
+                ] = measure_condition_code["attributes"]["description"]
