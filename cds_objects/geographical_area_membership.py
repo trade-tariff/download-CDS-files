42a6b4236b8721770c7c1e5206602881cde282cf
William Fish <william.michael.fish@gmail.com>
2022-10-03 17:18:52 +0100

    HOTT-2087: Lint geographical_area_membership

--- cds_objects/geographical_area_membership.py
+++ cds_objects/geographical_area_membership.py
@@ -1,10 +1,8 @@
-import csv
 from classes.master import Master
 import classes.globals as g
 
 
 class GeographicalAreaMembership(Master):
-
     def __init__(self, elem):
         Master.__init__(self, elem)
         self.elem = elem
@@ -13,19 +11,42 @@ class GeographicalAreaMembership(Master):
         self.get_data()
 
     def get_data(self):
-        self.validity_start_date = Master.process_null(self.elem.find("validityStartDate"))
+        self.validity_start_date = Master.process_null(
+            self.elem.find("validityStartDate")
+        )
         self.validity_end_date = Master.process_null(self.elem.find("validityEndDate"))
-        self.hjid_of_member = int(Master.process_null(self.elem.find("geographicalAreaGroupSid")))
+        self.hjid_of_member = int(
+            Master.process_null(self.elem.find("geographicalAreaGroupSid"))
+        )
         self.get_member_description()
         self.tbl = [
-               "Membership start date", Master.format_date(self.validity_start_date),
-               "HJID", self.hjid_of_member,
-               ]
+            "Membership start date",
+            Master.format_date(self.validity_start_date),
+            "HJID",
+            self.hjid_of_member,
+        ]
         if self.validity_end_date == "":
-            self.membership_string = Master.format_date(self.validity_start_date) + " : " + self.member_description + "\n"
-            self.current_membership_string = Master.format_date(self.validity_start_date) + " : " + self.member_description + "\n"
+            self.membership_string = (
+                Master.format_date(self.validity_start_date)
+                + " : "
+                + self.member_description
+                + "\n"
+            )
+            self.current_membership_string = (
+                Master.format_date(self.validity_start_date)
+                + " : "
+                + self.member_description
+                + "\n"
+            )
         else:
-            self.membership_string = Master.format_date(self.validity_start_date) + " to " + Master.format_date(self.validity_end_date) + " : " + self.member_description + "\n"
+            self.membership_string = (
+                Master.format_date(self.validity_start_date)
+                + " to "
+                + Master.format_date(self.validity_end_date)
+                + " : "
+                + self.member_description
+                + "\n"
+            )
 
     def get_member_description(self):
         self.member_description = g.geography_hjid_dict[self.hjid_of_member]
