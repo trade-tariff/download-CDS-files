fee931d49e2b934e02f3ee475fb15faf5afbaa52
William Fish <william.michael.fish@gmail.com>
2022-10-03 17:17:42 +0100

    HOTT-2087: Lint functions.py

--- classes/functions.py
+++ classes/functions.py
@@ -1,14 +1,14 @@
 def parse_date(d):
     d2 = d[6:8] + "/" + d[4:6] + "/" + d[0:4]
-    return (d2)
+    return d2
 
-def get_nodes(path):
-    pass
 
 def xml_to_xlsx_filename(filename):
     parts = filename.split("T")
     part0 = parts[0]
     parts = part0.split("-")
     part1 = parts[1]
-    excel_filename = "CDS updates " + part1[0:4] + "-" + part1[4:6] + "-" + part1[6:] + ".xlsx"
-    return (excel_filename)
+    excel_filename = (
+        "CDS updates " + part1[0:4] + "-" + part1[4:6] + "-" + part1[6:] + ".xlsx"
+    )
+    return excel_filename
