32588ed02d3e2c9e7410bd65a983ecb957d79592
William Fish <william.michael.fish@gmail.com>
2022-10-07 14:35:20 +0100

    HOTT-2087: Code review

--- classes/functions.py
+++ classes/functions.py
@@ -7,9 +7,7 @@ def xml_to_xlsx_filename(filename):
     return "CDS updates " + xml_to_file_date(filename) + ".xlsx"
 
 
-# export-20221006T000000_20221006T235959-20221007T001536.xml
 def xml_to_file_date(filename):
-    filename = "export-20221006T000000_20221006T235959-20221007T001536.xml"
     xml_date_part = filename.split("T")[0].split("-")[1]
 
     year = xml_date_part[0:4]
